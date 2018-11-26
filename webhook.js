21d890930b9821763bf6905622b71d695282b798
michikusa <michikusa.okuribito@gmail.com>
2018-11-26 20:27:44 +0900

    bug fixed

--- webhook.js
+++ webhook.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 1337),() => {
+app.listen(process.env.PORT || 5000),() => {
     console.log("runnning port");
 }
 
