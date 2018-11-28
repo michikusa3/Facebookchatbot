499736222e6b994bc1b35cc5f5940aafadbf7864
michikusa <michikusa.okuribito@gmail.com>
2018-11-28 18:46:53 +0900

    fixed

--- webhook.js
+++ webhook.js
@@ -11,31 +11,13 @@ app.listen(process.env.PORT || 5000),() => {
 }
 
 app.get('/',(req,res) => {
-    let VERIFY_TOKEN = 'texduo_cat';
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-    if(mode && token){
-        if(mode === 'subscribe' && token === VERIFY_TOKEN){
             console.log('WEBHOOK_VERIFY');
             res.status(200).send(challenge);
-        }else{
-            res.sendStatus(403);
-        }
-    }
 });
 
 app.post('/',(req,res) =>{
-    let body = req.body;
-    if (body.object === 'page'){
-        body.entry.forEach(function(entry) {
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
         res.status(200).send('EVENT_RECEIVED')
-    }else{
         res.sendStatus(404);
-    }
 });
 
 
