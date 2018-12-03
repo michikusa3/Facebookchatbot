23815f937a7e9a94e02ef5a564a9ce789e87b5f9
michikusa <michikusa.okuribito@gmail.com>
2018-12-03 20:31:22 +0900

    fixed

--- webhook.js
+++ webhook.js
@@ -1,30 +1,30 @@
 const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-
-app.use(bodyParser.urlencoded({extended : false}));
-app.use(bodyParser.json());
+const app = express().use(bodyParser.json());
 
 app.listen(process.env.PORT || 5000),() => {
     console.log("runnning port");
 }
+app.use(bodyParser.urlencoded({extended : false}));
+
+
 
 app.get('/',function(req,res) {
     console.log('WEBHOOK_VERIFY');
     res.sendStatus(200);
 });
 
-//'/'からGETは帰ってくるように成ったが、POSTが返ってこなくてランタイムエラー吐いてる
+/*//'/'からGETは帰ってくるように成ったが、POSTが返ってこなくてランタイムエラー吐いてる
 app.post('/',(checkUserAuth, findApp, renderView, sendJSON) =>{
     function checkUserAuth(req, res, next) {
         if (req.session.user) return next();
         return next(new NotAuthorizedError());
     }
-});
+});*/
 
 
-app.get('/webhook/',function(req,res){
+app.get('/webhook',function(req,res){
     let VERIFY_TOKEN = 'texduo_cat';
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
@@ -39,7 +39,7 @@ app.get('/webhook/',function(req,res){
     }
 });
 
-app.post('/webhook/',function(req,res){
+app.post('/webhook',function(req,res){
     let body = req.body;
     if (body.object === 'page'){
         body.entry.forEach(function(entry) {
